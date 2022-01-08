2f81dc236f52699cfeb2a475a5cb9ae397fa7790
Mylan Connolly <mconnolly@iotrak.com>
2022-01-08 09:08:51 -0500

    Add Devise, ActionText, and home controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,17 +2,12 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-
 ENV['RAILS_ENV'] ||= 'test'
-
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
-
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
-require 'support/factory_bot'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -30,25 +25,16 @@ require 'support/factory_bot'
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-
 RSpec.configure do |config|
-  config.include(ActiveSupport::Testing::TimeHelpers)
-
-  config.include(Shoulda::Matchers::ActiveModel, type: :model)
-  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
-
-  config.before(:each, type: :component) do
-    @request = controller.request
-  end
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
